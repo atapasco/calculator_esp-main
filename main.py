a4dfaf56b2ac04c3d31bfd3b2ef9d766bd7e6fa5
atapasco <atapasco@unicesar.edu.co>
2024-06-28 19:12:00 -0500

    actualizacion

--- main.py
+++ main.py
@@ -47,15 +47,13 @@ def form():
     return render_template('form.html')
 
 #Resultados del formulario
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET','POST'])
 def submit_form():
-    # Declarar variables para la recogida de datos
     name = request.form['name']
-
-    # Puedes guardar tus datos o enviarlos por correo electrónico
-    return render_template('form_result.html', 
-                           # Coloque aquí las variables
-                           name=name,
-                           )
+    email = request.form['email']
+    address = request.form['address']
+    date = request.form['date']
+    
+    return render_template('form_result.html', name=name, email=email, date = date, address = address)
 
 app.run(debug=True)
